beff4a5acfea1d9af751c978e9ab99ed5fd8ecb5
Tarun kumar <it.tarun07@gmail.com>
2021-08-24 02:13:04 +0530

    add categories

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
 
+import "assets/scss/styles.scss";
 import "assets/scss/material-kit-react.scss?v=1.10.0";
 
 // pages for this product
--- src/views/ProfilePage/ProfilePage.js
+++ src/views/ProfilePage/ProfilePage.js
@@ -18,7 +18,7 @@ import NavPills from "components/NavPills/NavPills.js";
 import Parallax from "components/Parallax/Parallax.js";
 import Card from "components/Card/Card.js";
 
-import profile from "assets/img/faces/christian.jpg";
+import profile from "assets/img/renu.jpeg";
 
 import studio1 from "assets/img/studio/1.jpeg";
 import studio2 from "assets/img/studio/2.jpeg";
@@ -32,6 +32,28 @@ import studio9 from "assets/img/studio/9.jpeg";
 import studio10 from "assets/img/studio/10.jpeg";
 import studio11 from "assets/img/studio/11.jpeg";
 
+import product1 from "assets/img/products/1.jpg";
+import product2 from "assets/img/products/2.jpg";
+import product3 from "assets/img/products/3.jpg";
+import product4 from "assets/img/products/4.jpg";
+import product5 from "assets/img/products/5.jpeg";
+import product6 from "assets/img/products/6.jpg";
+import product7 from "assets/img/products/7.jpg";
+import product8 from "assets/img/products/8.jpg";
+import product9 from "assets/img/products/9.jpg";
+import product10 from "assets/img/products/10.jpg";
+import product11 from "assets/img/products/11.jpg";
+
+import fabric1 from "assets/img/fabric/1.JPG";
+import fabric2 from "assets/img/fabric/2.JPG";
+import fabric3 from "assets/img/fabric/3.JPG";
+import fabric4 from "assets/img/fabric/4.JPG";
+import fabric5 from "assets/img/fabric/5.JPG";
+import fabric6 from "assets/img/fabric/6.JPG";
+import fabric7 from "assets/img/fabric/7.JPG";
+import fabric8 from "assets/img/fabric/8.JPG";
+import fabric9 from "assets/img/fabric/8.JPG";
+
 import work1 from "assets/img/work/1.jpeg";
 import work2 from "assets/img/work/2.jpeg";
 import work3 from "assets/img/work/3.jpeg";
@@ -40,6 +62,44 @@ import work5 from "assets/img/work/5.jpeg";
 import work6 from "assets/img/work/6.jpeg";
 import work7 from "assets/img/work/7.jpeg";
 import work8 from "assets/img/work/8.jpeg";
+
+const studioData = [
+  { src: studio1 },
+  { src: studio2 },
+  { src: studio3 },
+  { src: studio4 },
+  { src: studio5 },
+  { src: studio6 },
+  { src: studio7 },
+  { src: studio8 },
+  { src: studio9 },
+  { src: studio10 },
+  { src: studio11 },
+];
+const productsData = [
+  { src: product1 },
+  { src: product2 },
+  { src: product3 },
+  { src: product4 },
+  { src: product5 },
+  { src: product6 },
+  { src: product7 },
+  { src: product8 },
+  { src: product9 },
+  { src: product10 },
+  { src: product11 },
+];
+const fabricData = [
+  { src: fabric1 },
+  { src: fabric3 },
+  { src: fabric2 },
+  { src: fabric4 },
+  { src: fabric5 },
+  { src: fabric6 },
+  { src: fabric7 },
+  { src: fabric8 },
+  { src: fabric9 },
+];
 
 import styles from "assets/jss/material-kit-react/views/profilePage.js";
 
@@ -99,91 +159,19 @@ export default function ProfilePage(props) {
                       tabButton: "Studio",
                       tabIcon: Camera,
                       tabContent: (
-                        <GridContainer justify="center">
-                          <GridItem xs={12} sm={12} md={6}>
-                            <Card>
-                              <img
-                                alt="..."
-                                src={studio1}
-                                className={navImageClasses}
-                              />
-                            </Card>
-                            <Card>
-                              <img
-                                alt="..."
-                                src={studio5}
-                                className={navImageClasses}
-                              />
-                            </Card>
-                          </GridItem>
-                          <GridItem xs={12} sm={12} md={6}>
-                            <Card>
-                              <img
-                                alt="..."
-                                src={studio3}
-                                className={navImageClasses}
-                              />
-                            </Card>
-
-                            <Card>
-                              <img
-                                alt="..."
-                                src={studio2}
-                                className={navImageClasses}
-                              />
-                            </Card>
-                          </GridItem>
-                          <GridItem xs={12} sm={12} md={6}>
-                            <Card>
-                              <img
-                                alt="..."
-                                src={studio6}
-                                className={navImageClasses}
-                              />
-                            </Card>
-                            <Card>
-                              <img
-                                alt="..."
-                                src={studio7}
-                                className={navImageClasses}
-                              />
-                            </Card>
-                          </GridItem>
-                          <GridItem xs={12} sm={12} md={6}>
-                            <Card>
-                              <img
-                                alt="..."
-                                src={studio8}
-                                className={navImageClasses}
-                              />
-                            </Card>
-                            <Card>
-                              <img
-                                alt="..."
-                                src={studio9}
-                                className={navImageClasses}
-                              />
-                            </Card>
-                          </GridItem>
-                          <GridItem xs={12} sm={12} md={6}>
-                            <Card>
-                              <img
-                                alt="..."
-                                src={studio10}
-                                className={navImageClasses}
-                              />
-                            </Card>
-                          </GridItem>
-                          <GridItem xs={12} sm={12} md={6}>
-                            <Card>
-                              <img
-                                alt="..."
-                                src={studio11}
-                                className={navImageClasses}
-                              />
-                            </Card>
-                          </GridItem>
-                        </GridContainer>
+                        <div className="grid-section grid-image-fr">
+                          {studioData.map((item, index) => {
+                            return (
+                              <Card key={index} className="grid-card">
+                                <img
+                                  alt="..."
+                                  src={item.src}
+                                  className={navImageClasses}
+                                />
+                              </Card>
+                            );
+                          })}
+                        </div>
                       ),
                     },
                     {
@@ -275,24 +263,45 @@ export default function ProfilePage(props) {
                               className={navImageClasses}
                             />
                           </GridItem>
-                          <GridItem xs={12} sm={12} md={6}>
-                            <img
-                              alt="..."
-                              src={work2}
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src={work1}
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src={studio1}
-                              className={navImageClasses}
-                            />
-                          </GridItem>
                         </GridContainer>
+                      ),
+                    },
+                    {
+                      tabButton: "Products",
+                      tabIcon: Favorite,
+                      tabContent: (
+                        <div className="grid-section grid-image-fr">
+                          {productsData.map((item, index) => {
+                            return (
+                              <Card key={index} className="grid-card">
+                                <img
+                                  alt="..."
+                                  src={item.src}
+                                  className={navImageClasses}
+                                />
+                              </Card>
+                            );
+                          })}
+                        </div>
+                      ),
+                    },
+                    {
+                      tabButton: "Fabric",
+                      tabIcon: Favorite,
+                      tabContent: (
+                        <div className="grid-section grid-image-fr">
+                          {fabricData.map((item, index) => {
+                            return (
+                              <Card key={index} className="grid-card">
+                                <img
+                                  alt="..."
+                                  src={item.src}
+                                  className={navImageClasses}
+                                />
+                              </Card>
+                            );
+                          })}
+                        </div>
                       ),
                     },
                   ]}
